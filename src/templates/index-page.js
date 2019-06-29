f826fffdbb81d0c5beb90b2358a5d1f3c961543d
Paul Dixon <paullydca@gmail.com>
2019-06-29 11:14:08 -0300

    Update index-page.js
    updated title css

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,10 +39,7 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+            color: '0D2538',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -52,10 +49,7 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+            color: '0D2538',
             lineHeight: '1',
             padding: '0.25em',
           }}
