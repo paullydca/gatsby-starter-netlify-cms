d2bd0f2e73f5ee3691299d47fa12a9a18d27ad98
Paul Dixon <paullydca@gmail.com>
2019-06-29 11:44:23 -0300

    Update Footer.js
    4 column footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,16 +11,19 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
+        
+        <div className="content has-text-centered has-background-black has-text-white-ter">
+          <div className="container has-background-black has-text-white-ter">
+            <div className="columns">
+            <div className="column is-4">
+            <div className="content has-text-centered">
           <img
             src={logo}
             alt="Kaldi"
             style={{ width: '14em', height: '2em' }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
+              </div>
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
